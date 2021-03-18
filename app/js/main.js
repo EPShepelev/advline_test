501ea07b474733f023c9de1cce3bf95400e3410d
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-18 11:38:57 +0300

    created background gradient

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
 $(function () {
   $(".slider__items").slick({
     dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+    focusOnSelect: true,
   });
 });
