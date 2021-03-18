9bf4aef538b8825e1dd1d155d2a30a21264b68ea
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-18 15:51:43 +0300

    changed fade speed for slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,10 @@
 $(function () {
   $(".slider__items").slick({
     dots: true,
-    // slidesToShow: 1,
-    // slidesToScroll: 1,
     fade: true,
     cssEase: "linear",
     arrows: false,
     focusOnSelect: true,
+    speed: 1300,
   });
 });
